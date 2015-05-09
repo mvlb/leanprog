f11fcdaadf7a9e6ba01592a27a0c65c7816204f5
Balaji Ravindran <coderb84@gmail.com>
2015-05-08 21:16:19 -0700

    Initial Commit

---
+++ leanprog/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  leanprog
+//
+//  Created by Balaji Ravindran on 5/8/15.
+//  Copyright (c) 2015 Balaji Ravindran. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
